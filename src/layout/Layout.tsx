283bd4c435757ff4b1f9507bc8ee4c3592f170e2
Admin <1119321.ab@mail.ru>
2025-02-22 10:18:56 +0100

    lesson15

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,7 +1,7 @@
 import { NavLink, Outlet } from 'react-router-dom'
 import styles from './layout.module.css'
-import Header from './Header';
-import Footer from './Footer';
+// import Header from './Header';
+// import Footer from './Footer';
 import { getTotalPrice } from '../components/cart/Cart';
 import { useCart } from '../context/CartContext';
 
