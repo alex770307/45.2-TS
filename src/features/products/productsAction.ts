466f28da6f3ae7020c100cf85eda245403d938d2
Admin <1119321.ab@mail.ru>
2025-03-07 20:49:54 +0100

    dyck

--- src/features/products/productsAction.ts
+++ src/features/products/productsAction.ts
@@ -15,13 +15,21 @@ export const loadProducts = createAsyncThunk(
 );
 
 export const loadLimitedProducts = createAsyncThunk(
-    'products/loadLimitedProducts',
-    async (limit: number, thunkAPI) => {
-      try {
-        const response = await axios.get(`https://fakestoreapi.com/products?limit=${limit}`);
-        return response.data;
-      } catch (error: any) {
-        return thunkAPI.rejectWithValue(error.message);
-      }
+    'productsLimit/loadLimitedProducts',
+    async (limit: number) => {
+      const response = await fetch(`https://fakestoreapi.com/products?limit=${limit}`);
+      return await response.json();
     }
   );
+
+// export const loadLimitedProducts = createAsyncThunk(
+//     'productsLimit/loadLimitedProducts',
+//     async (limit: number, thunkAPI) => {
+//       try {
+//         const response = await axios.get(`https://fakestoreapi.com/products?limit=${limit}`);
+//         return response.data;
+//       } catch (error: any) {
+//         return thunkAPI.rejectWithValue(error.message);
+//       }
+//     }
+//   );
